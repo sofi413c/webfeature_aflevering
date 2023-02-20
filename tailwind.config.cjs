53da82bc3c6c62e6ca61c58f5659afc2d7993bc0
Sofie Hemmingsen <sofi413c@stud.kea.dk>
2023-02-20 16:36:53 +0100

    colorpalette done

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,6 +2,18 @@
 module.exports = {
   content: ["./src/**/*.{html,vue,js,ts,jsx,tsx}"],
   theme: {
+    colors: {
+      p_grey: "#858585",
+      p_mild_grey: "#D9D9D9",
+      p_light_grey: "#ECECEF",
+      s_black: "#292929",
+      s_brown: "#735039",
+      s_olive: "#747163",
+      s_beige: "#ECDEB5",
+      s_yellow: "#FCFF54",
+      s_turquoise: "#9DE6DA",
+      s_white: "#FFFFFF",
+    },
     extend: {},
   },
   plugins: [require("@tailwindcss/typography"), require("@tailwindcss/forms")],
